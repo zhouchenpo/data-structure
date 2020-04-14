2521c95fdbd1d8bd382231d5012eacdf8b51ef1c
zhouchenpo <zhouchenpo@zju.edu.cn>
2020-04-14 22:09:22 +0800

    4/14

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args){
-        SinglyLinkedList myList = new SinglyLinkedList(1);
-        myList.setData(1);
-        myList.addData(2);
-        myList.addData(3);
-        myList.addData(3);
+        SinglyLinkedList myList = new SinglyLinkedList(new int[]{1, 2, 3, 4});
         myList.printAll();
+        System.out.println();
+        myList.addData(new int[]{5, 6, 7, 8});
+        myList.printAll();
+
     }
 }
--- src/SinglyLinkedList.java
+++ src/SinglyLinkedList.java
@@ -3,15 +3,29 @@ public class SinglyLinkedList {
     private SinglyLinkedList next = null;
 
     public SinglyLinkedList() {
-
     }
 
     public SinglyLinkedList(int data) {
         this.data = data;
     }
 
+    public SinglyLinkedList(int[] nums) {
+        SinglyLinkedList temp = this;
+        if (nums.length >= 1) {
+            temp.data = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                temp.next = new SinglyLinkedList(nums[i]);
+                temp = temp.next;
+            }
+        }
+    }
+
     public void setData(int data) {
         this.data = data;
+    }
+
+    public int getData() {
+        return this.data;
     }
 
     public SinglyLinkedList getNext() {
@@ -20,17 +34,36 @@ public class SinglyLinkedList {
 
     public void addData(int data) {
         SinglyLinkedList temp = this;
-        while (temp.getNext() != null) {
-            temp = temp.getNext();
+        while (temp.next != null) {
+            temp = temp.next;
         }
         temp.next = new SinglyLinkedList(data);
+    }
+
+    public void addData(int[] nums) {
+        SinglyLinkedList temp = this;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        if (nums.length >= 1) {
+            for (int num : nums) {
+                temp.next = new SinglyLinkedList(num);
+                temp = temp.next;
+            }
+        }
     }
 
     public void printAll() {
         SinglyLinkedList temp = this;
         while (temp != null) {
             System.out.print(temp.data);
-            temp = temp.getNext();
+            temp = temp.next;
         }
+    }
+
+    public void reverse() {
+    }
+
+    public void deleteAll(int data) {
     }
 }
